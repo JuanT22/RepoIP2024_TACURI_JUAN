2617481b9444d441cd0306684c09348b40a0c3df
Juan Tacuri <tacurijuanito28@gmail.com>
2024-11-20 23:14:26 -0500

    Update and rename ValidarOrden1.java to BaseCode.java

---
+++ CodigoJavaBase/BaseCode.java
@@ -0,0 +1,7 @@
+package basecode;
+import java.util.Scanner;
+public class BaseCode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);         
+    }    
+}
--- CodigoJavaBase/ValidarOrden1.java
+++
@@ -1,9 +0,0 @@
-package validarorden1;
-import java.util.Scanner;
-public class ValidarOrden1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
-        
-    }
-    
-}
